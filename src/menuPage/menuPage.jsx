fe7c1699c4b4cf9b01bc4955a2d237758b91d32b
cmyweb <cmyweb@yahoo.com>
2020-05-20 10:57:09 +0100

    progess

--- src/menuPage/menuPage.jsx
+++ src/menuPage/menuPage.jsx
@@ -1,14 +1,14 @@
 import React, { useContext } from "react";
 import MenuCard from "./menuPageCard";
 import { context } from "../context";
-import "./menupage.css";
+import styles from "./menupage.module.css";
 const MenuPage = () => {
   const { data } = useContext(context);
   console.log(data, "in menu page");
   return (
     <div>
       <h1>in menu page</h1>
-      <div className="wrap">
+      <div className={styles.wrap}>
         {data.map((x) => (
           <MenuCard price={x.productPrice} key={x.id} name={x.productName} />
         ))}
