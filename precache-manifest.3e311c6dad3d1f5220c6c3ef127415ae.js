b60b76cc79bad1ac2252a505a1a80725b7126197
cmyweb <cmyweb@yahoo.com>
2020-06-01 00:33:46 +0100

    Updates

---
+++ precache-manifest.3e311c6dad3d1f5220c6c3ef127415ae.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c4a56a95bb6d0a8c047afe0d211331a",
+    "url": "/svlunchboxncocktails/index.html"
+  },
+  {
+    "revision": "6ed3beec8ec8584cac9e",
+    "url": "/svlunchboxncocktails/static/css/2.1d9a7d86.chunk.css"
+  },
+  {
+    "revision": "762a20fa0ba5095085ea",
+    "url": "/svlunchboxncocktails/static/css/main.0aa1e7ff.chunk.css"
+  },
+  {
+    "revision": "6ed3beec8ec8584cac9e",
+    "url": "/svlunchboxncocktails/static/js/2.165f69b9.chunk.js"
+  },
+  {
+    "revision": "cc539bb4a85cbe0e42d76df177ff76fc",
+    "url": "/svlunchboxncocktails/static/js/2.165f69b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "762a20fa0ba5095085ea",
+    "url": "/svlunchboxncocktails/static/js/main.6c1dbb73.chunk.js"
+  },
+  {
+    "revision": "24447abd789c1831ba97",
+    "url": "/svlunchboxncocktails/static/js/runtime-main.0bde2342.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/svlunchboxncocktails/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/svlunchboxncocktails/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/svlunchboxncocktails/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/svlunchboxncocktails/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/svlunchboxncocktails/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/svlunchboxncocktails/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/svlunchboxncocktails/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/svlunchboxncocktails/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/svlunchboxncocktails/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/svlunchboxncocktails/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/svlunchboxncocktails/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/svlunchboxncocktails/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/svlunchboxncocktails/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/svlunchboxncocktails/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/svlunchboxncocktails/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/svlunchboxncocktails/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
